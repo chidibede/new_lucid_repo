5fb3d7c5ea7ec4205407d473426a1ff762aa02cb
Chidi Enwereji <chidibede@gmail.com>
2019-11-03 17:49:04 +0100

    updated static and template folders

--- deploy_interesting_feeds_model/app.py
+++ deploy_interesting_feeds_model/app.py
@@ -1,3 +1,6 @@
+''' Deploy Interesting feeds system as a web app
+    and API
+'''
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from pandas.io.json import json_normalize
@@ -7,94 +10,105 @@ import numpy as np
 app = Flask(__name__)
 
 def load_datasets(data):
+    '''Function to load the dataset
+    to be used
+    '''
     data = pd.read_csv(data)
     return data
 
 def select_columns(data, columns):
+    '''Function to select columns needed
+    '''
     data = data.loc[:, columns]
     return data
 
 # Load the data
-posts = load_datasets('posts.csv')
+POSTS = load_datasets('posts.csv')
 
 # Drop duplicates
-posts = posts.drop_duplicates()
+POSTS = POSTS.drop_duplicates()
 
 def clean_post_data():
     '''
      Function that cleans the post data using regex
     '''
     # Remove html tags
-    posts['content'] = posts['content'].str.replace(r'<[^>]*>', '')
+    POSTS['content'] = POSTS['content'].str.replace(r'<[^>]*>', '')
 
     # Remove white spaces including new lines
-    posts['content'] = posts['content'].str.replace(r'\s', ' ')
+    POSTS['content'] = POSTS['content'].str.replace(r'\s', ' ')
 
     # Remove square brackets
-    posts['content'] = posts['content'].str.replace(r'\[.*?\]', '')
+    POSTS['content'] = POSTS['content'].str.replace(r'\[.*?\]', '')
 
     # Remove image files
-    posts['content'] = posts['content'].str.replace(r'\(.*?\)', '')
-
-def filter_post_length(length):
-    '''
-    Function that allows us to choose the length we deem short
-    '''
-    top_posts = merged_posts[merged_posts['content'].str.len() > length].reset_index(drop=True)
-    return top_posts
+    POSTS['content'] = POSTS['content'].str.replace(r'\(.*?\)', '')
 
 # Load the exported system
-top_feeds = joblib.load('model.sav')
+TOP_FEEDS = joblib.load('model.sav')
 
 # clean post data
 clean_post_data()
 
 # Select needed columns
-posts = select_columns(posts, ['id', 'title', 'content']) 
+POSTS = select_columns(POSTS, ['id', 'title', 'content'])
 
 # Rename columns
-posts.columns = ['post_id', 'title', 'content']
+POSTS.columns = ['post_id', 'title', 'content']
 
 # Merge the top feeds with the posts
-merged_posts = pd.merge(posts, top_feeds, on='post_id').drop_duplicates()
+MERGED_POSTS = pd.merge(POSTS, TOP_FEEDS, on='post_id').drop_duplicates()
 
 # sort
-merged_posts = merged_posts.sort_values(by='ratings', ascending=False)
+MERGED_POSTS = MERGED_POSTS.sort_values(by='ratings', ascending=False)
+
+def filter_post_length(length):
+    '''
+    Function that allows us to choose the length we deem short
+    '''
+    top_posts = MERGED_POSTS[MERGED_POSTS['content'].str.len() > length].reset_index(drop=True)
+    return top_posts
+
 
 # remove short posts
-top_posts = filter_post_length(150)
+TOP_POSTS = filter_post_length(150)
 
 # top 5 posts
-top_5_posts = np.array(top_posts.head().loc[:, ['title','content', 'ratings']])
+TOP_5_POSTS = np.array(TOP_POSTS.head().loc[:, ['title', 'content', 'ratings']])
 
 # Top 8 posts
-top_8_posts = np.array(top_posts.head(8).loc[:, ['title','content', 'ratings']])
+TOP_8_POSTS = np.array(TOP_POSTS.head(8).loc[:, ['title', 'content', 'ratings']])
 
 @app.route('/')
 def home():
+    '''Function to render the home page'''
     return render_template('index.html')
 
 @app.route('/grading_method')
 def grading_method():
+    '''Function to render the grading page'''
     return render_template('grading_method.html')
 
 @app.route('/api_documentation')
 def api_documentation():
+    '''Function to render the docs page'''
     return render_template('api_documentation.html')
 
 @app.route('/top_5_feeds')
 def top_5_feeds():
+    '''Function to return the top 5 posts'''
     try:
-        return render_template('top_5_feeds.html', top_5_posts=top_5_posts)
+        return render_template('top_5_feeds.html', top_5_posts=TOP_5_POSTS)
     except:
-        return render_template('top_5_feeds.html', top_5_posts = "No top posts")
+        return render_template('top_5_feeds.html', top_5_posts="No top posts")
 
 @app.route('/top_8_feeds')
 def top_8_feeds():
+    '''Function to return the top 8 posts'''
     try:
-        return render_template('top_8_feeds.html', top_8_posts=top_8_posts)
+        return render_template('top_8_feeds.html', top_8_posts=TOP_8_POSTS)
     except:
-        return render_template('top_8_feeds.html', top_8_posts = "No top posts")
+        return render_template('top_8_feeds.html', top_8_posts="No top posts")
 
 @app.route('/top_feeds_api', methods=['POST'])
 def top_feeds_api():
@@ -104,11 +118,12 @@ def top_feeds_api():
         json_data = request.get_json(force=True)
         json_df = json_normalize(json_data)
         for feed in json_df.feeds:
-            number_of_feed =int(feed)
-       
+            number_of_feed = int(feed)
+
        # top posts
         top_posts_api = filter_post_length(150)
-        top_posts_api = np.array(top_posts_api.head(number_of_feed).loc[:, ['title','content', 'ratings']])
+        select_cols = ['title', 'content', 'ratings']
+        top_posts_api = np.array(top_posts_api.head(number_of_feed).loc[:, select_cols])
 
         interesting_feeds = {
             'interesting_feeds': [[post[0], post[1], post[2]] for post in top_posts_api],
